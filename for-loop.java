7e639b5c5e0837ea07268f2277cf76b8c12310ed
Ahmed_Hussieny <119592011+Ahmed-Hussieny@users.noreply.github.com>
2023-08-31 09:42:27 +0300

    for-loop

---
+++ for-loop.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        * for loop = executes a block of code a limited amount of times
+        */
+
+//        ascending  0 => 10
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+//        descending  10 => 0
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+
+//        if we need to cou nt down by two
+      
+        for (int i = 10; i >= 0; i-=2) {
+            System.out.println(i);
+        }
+
+    }
+}
